fdc52a1acfb422860d4d99930d91c67ac2610bbd
Gleomit <draychev95@mail.ru>
2014-09-13 22:22:59 +0300

    Update GamePanel.java

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,7 +1,14 @@
 package main;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -11,16 +18,19 @@ public class GamePanel extends JPanel implements ActionListener {
 
     private Timer timer;
 
-    Rectangle tempRectangle;
+    Character character;
     int velx = 0, vely = 0;
+    Map map;
     public GamePanel() {
 
         addKeyListener(new InputHandler());
         setFocusable(true);
         setBackground(Color.BLACK);
         setDoubleBuffered(true);
-
-        tempRectangle = new Rectangle(0,0,40,40);
+        
+        map = new Map();
+        System.out.println(map.tiles.size());
+        character = new Character();
         timer = new Timer(5, this);
         timer.start();
     }
@@ -30,8 +40,11 @@ public class GamePanel extends JPanel implements ActionListener {
         super.paint(g);
 
         Graphics2D g2d = (Graphics2D)g;
-        g2d.fillRect(tempRectangle.x, tempRectangle.y,40,40);
-
+      //  g2d.fillRect(tempRectangle.x, tempRectangle.y,40,40);
+        
+        map.drawMap(g);
+        g2d.drawImage(character.currentImage, character.positionX, character.positionY, this);
+       
         Toolkit.getDefaultToolkit().sync();
         g.dispose();
     }
@@ -39,8 +52,12 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void actionPerformed(ActionEvent e) 
     {
-    	tempRectangle.x += velx;
-    	tempRectangle.y += vely;
+    	character.positionX += velx;
+    	//tempRectangle.y += vely;
+    	if(character.positionY < 480 - 72)
+    	{
+    		character.positionY += 4;
+    	}
         repaint();  
     }
     
@@ -62,7 +79,7 @@ public class GamePanel extends JPanel implements ActionListener {
 
             if (key == KeyEvent.VK_UP) 
             {
-            	vely = -1;
+            	//tempRectangle.y = -80;
             }
 
             if (key == KeyEvent.VK_DOWN)

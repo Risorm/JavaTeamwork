f1644972e7796309a13a5260dfa3b6a820847109
Krastyo <krastio.krastev@gmail.com>
2014-09-13 21:18:18 +0300

    changed libraries

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,14 +1,7 @@
 package main;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;

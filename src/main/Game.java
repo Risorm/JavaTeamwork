637f5a181b331afeb6add1c274d06c76e30f08a5
Velizar Bishurov <vbishurov@gmail.com>
2014-09-13 14:34:25 +0300

    package main

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,36 +1,35 @@
 package main;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
-public class Game extends JFrame
-{
-	public Game() 
-	{
+public class Game extends JFrame {
+
+	private static boolean running = false;
+
+	public Game() {
 		initGame();
 	}
-	
-	private void initGame()
-	{
+
+	private void initGame() {
 		add(new GamePanel());
 		setResizable(false);
-		
+
 		pack();
-		
+
 		setTitle(GamePanel.TITLE);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
-	
-	public static void main(String[] args) 
-	{
-		EventQueue.invokeLater(new Runnable()
-		{
-			public void run() 
-			{
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
 				JFrame game = new Game();
 				game.setVisible(true);
+				game.setFocusable(true);
 			}
 		});
 	}

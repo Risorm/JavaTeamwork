bb44c9c59605980f770ed7a0425be4231229335d
Gleomit <draychev95@mail.ru>
2014-09-13 23:33:52 +0300

    Update Character.java

--- src/main/Character.java
+++ src/main/Character.java
@@ -1,43 +1,64 @@
 package main;
 
-import java.awt.*;
+import java.awt.Image;
 import java.util.LinkedList;
 
-public class Character {
+public class Character 
+{
 	public int positionX;
 	public int positionY;
-
-	int currentFrame;
-
+	
+	int currentFrame;	
+	
 	LinkedList<Image> walkRightAnimation;
 	LinkedList<Image> walkLeftAnimation;
 	Image idle;
-
+	
 	public Image currentImage;
 	boolean walkingRight;
 	boolean walkingLeft;
 	boolean stay;
-
-	public void initImages() {
+	
+	public void initImages()
+	{
 		walkRightAnimation = new LinkedList<>();
 		walkLeftAnimation = new LinkedList<>();
-
-		for (int i = 1; i <= 3; i++) {
-			walkRightAnimation.add(Utils.loadImage("res/walkRight" + i
-					+ ".png"));
-			walkLeftAnimation.add(Utils.loadImage("res/walkLeft" + i
-					+ ".png"));
+		
+		for (int i = 1; i <= 3 ; i++)
+		{
+			walkRightAnimation.add(Utils.loadImage("walkRight" + i + ".png"));
+			walkLeftAnimation.add(Utils.loadImage("resources/walkLeft" + i + ".png"));
 		}
-		idle = Utils.loadImage("res/idle.png");
+		idle = Utils.loadImage("idle.png");
 	}
-
-	public void update() {
-		if (stay == true) {
+	
+	public void update()
+	{
+		if(stay == true)
+		{
 			currentImage = idle;
 		}
+		else if(walkingRight == true)
+		{
+			currentImage = walkRightAnimation.get(currentFrame);
+			currentFrame++;
+		}
+		else if(walkingLeft == true)
+		{
+			currentImage = walkLeftAnimation.get(currentFrame);
+			currentFrame++;
+		}
+		else if(walkingRight == false && walkingLeft == false)
+		{
+			currentImage = idle;
+			currentFrame = 0;
+		}
+		if(currentFrame >= 3)
+			currentFrame = 0;
 	}
-
-	public Character() {
+	
+	public Character() 
+	{
 		initImages();
 		positionX = 0;
 		positionX = 0;

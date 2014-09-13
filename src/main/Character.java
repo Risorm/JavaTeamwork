1726b9a5b33dfe028abf81365a4ec2f15c004b2c
Gleomit <draychev95@mail.ru>
2014-09-13 16:39:52 +0300

    Create Character.java
    "resources" е временен път към фаиловете, все още не сме направили анимациите иначе конструкцията може да я ъпгрейдваме

---
+++ src/main/Character.java
@@ -0,0 +1,43 @@
+import java.awt.Image;
+import java.util.LinkedList;
+
+public class Character 
+{
+	public int positionX;
+	public int positionY;
+	
+	int currentFrame;	
+	
+	LinkedList<Image> walkRightAnimation;
+	LinkedList<Image> walkLeftAnimation;
+	Image idle;
+	
+	public Image currentImage;
+	boolean walkingRight;
+	boolean walkingLeft;
+	boolean stay;
+	
+	public void initImages()
+	{
+		walkRightAnimation = new LinkedList<>();
+		walkLeftAnimation = new LinkedList<>();
+		
+		for (int i = 1; i <= 3 ; i++)
+		{
+			walkRightAnimation.add(Utils.loadImage("resources/walkRight" + i + ".png"));
+			walkLeftAnimation.add(Utils.loadImage("resources/walkLeft" + i + ".png"));
+		}
+		idle = Utils.loadImage("resources/idle.png");
+	}
+	
+	public Character() 
+	{
+		initImages();
+		positionX = 0;
+		positionX = 0;
+		walkingLeft = walkingRight = false;
+		stay = true;
+		currentFrame = 0;
+		currentImage = idle;
+	}
+}

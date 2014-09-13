c5713a6084db8e60b5a3ee7db2a375fcd1f26e3d
Gleomit <draychev95@mail.ru>
2014-09-13 20:24:05 +0300

    Update Game.java

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,30 +1,22 @@
-package main;
-
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
-public class Game extends JFrame
+public class Game extends JFrame 
 {
-	public static final String TITLE = "AeroBlue-Platformer";
-	static JFrame window;
-	
-	public Game() 
-	{
-		initGame();
-	}
-	
-	static private void initGame()
-	{
-		window = new JFrame(TITLE);
-		window.setContentPane(new GamePanel());
-		window.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		window.setResizable(false);
-		window.pack();
-		window.setVisible(true);
-	}
-	public static void main(String[] args) 
-	{
-		initGame();
-	}
-}
+    public static final String TITLE = "AeroBlue-Platformer";
+    public Game() 
+    {
+        add(new GamePanel());
 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(640, 480);
+        setLocationRelativeTo(null);
+        setTitle(TITLE);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Game();
+    }
+}

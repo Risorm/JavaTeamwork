4bcfe415de646b2e0ed25417ae6282be7595ba61
Gleomit <draychev95@mail.ru>
2014-09-13 20:25:47 +0300

    Update GamePanel.java

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,125 +1,83 @@
-package main;
-
-import java.awt.Dimension;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
-
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-@SuppressWarnings("serial")
-public class GamePanel extends JPanel implements Runnable, KeyListener
-{
-	public static final int WIDTH = 640, HEIGHT = 480;
-	
-	private Thread thread;
-	private int FPS = 60;
-	private long targetTime = 1000 / FPS;
-	
-	private Graphics2D mainGraphics2d;
-	
-	public boolean running;
-	
-	Rectangle rectangle2d;
-	
-	public GamePanel() 
-	{
-		super();
-		initGamePanel();
-	}
-	
-	public void run() 
-	{
-		initTheGame();
-		rectangle2d = new Rectangle(0,0,40,40);
-		long start = 0;
-		long elapsed;
-		long wait;
-		
-		while(running)
-		{
-			update();
-			drawAll();
-			elapsed = System.nanoTime() - start;
-			wait = targetTime - elapsed / 1000000;
-			
-			try 
-			{
-				Thread.sleep(wait);
-			} 
-			catch (Exception e) 
-			{
-				e.printStackTrace();
-			}
-			
-		}
-	}
-	
-	private void update()
-	{
-		
-	}
-	
-	public void drawAll()
-	{
-		//drawHere
-		Graphics graphics = getGraphics();
-		graphics.drawRect(rectangle2d.x, rectangle2d.y, 40, 40);
-		repaint();
-		graphics.dispose();
-		
-	}
-	
-	
-	private void initGamePanel()
-	{
-		setPreferredSize(new Dimension(WIDTH,HEIGHT));
-		setFocusable(true);
-		requestFocus(true);
-		setDoubleBuffered(true);
-	}
-	
-	private void initTheGame()
-	{
-		mainGraphics2d = (Graphics2D) mainGraphics2d;
-		running = true;
-	}
-	
-	@Override
-	public void addNotify() 
-	{
-		super.addNotify();
-		if(thread == null)
-		{
-			thread = new Thread(this);
-			addKeyListener(this);
-			thread.start();
-		}
-	}
-	
-	@Override
-	public void keyPressed(KeyEvent e) 
-	{
-		int keyCode = e.getKeyCode();
-		if(keyCode == KeyEvent.VK_A)
-		{
-			rectangle2d.x --;
-		}
-		else if(keyCode == KeyEvent.VK_D)
-		{
-			rectangle2d.x ++;
-		}
-	}
-	@Override
-	public void keyReleased(KeyEvent e)
-	{
-		int keyCode = e.getKeyCode();
-		if(keyCode == KeyEvent.VK_A)
-		{
-			
-		}
-	}
+
+public class GamePanel extends JPanel implements ActionListener {
+
+    private Timer timer;
+
+    Rectangle tempRectangle;
+    
+    public GamePanel() {
+
+        addKeyListener(new InputHandler());
+        setFocusable(true);
+        setBackground(Color.BLACK);
+        setDoubleBuffered(true);
+
+        tempRectangle = new Rectangle(0,0,40,40);
+        timer = new Timer(5, this);
+        timer.start();
+    }
+
+
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.fillRect(tempRectangle.x, tempRectangle.y,40,40);
+
+        Toolkit.getDefaultToolkit().sync();
+        g.dispose();
+    }
+
+
+    public void actionPerformed(ActionEvent e) 
+    {
+        repaint();  
+    }
+    
+    private class InputHandler extends KeyAdapter
+    {
+        public void keyReleased(KeyEvent e) 
+        {
+            int key = e.getKeyCode();
+
+            if (key == KeyEvent.VK_LEFT)
+            {
+                tempRectangle.x += -1;
+            }
+
+            if (key == KeyEvent.VK_RIGHT) 
+            {
+            	tempRectangle.x += 1;
+            }
+
+            if (key == KeyEvent.VK_UP) 
+            {
+            	tempRectangle.y += -1;
+            }
+
+            if (key == KeyEvent.VK_DOWN)
+            {
+            	tempRectangle.y += 1;
+            }
+        }
+
+        public void keyPressed(KeyEvent e)
+        {
+            
+        }
+    }
+
 }

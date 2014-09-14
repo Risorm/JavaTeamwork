626040d840988585502d0ed4d9fef4b9eafe1427
Krastyo <krastio.krastev@gmail.com>
2014-09-14 15:24:06 +0300

    background and Enemy animations

--- src/main/Game.java
+++ src/main/Game.java
@@ -4,13 +4,14 @@ import javax.swing.JFrame;
 public class Game extends JFrame 
 {
 	
-	public static final int WIDTH = 640, HEIGHT = 480;
+	public static final int WIDTH = 320, HEIGHT = 240;
+	public static final int SCALE = 2;
     public Game() 
     {
         add(new GamePanel());
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(WIDTH, HEIGHT);
+        setSize(WIDTH*SCALE, HEIGHT*SCALE);
         setLocationRelativeTo(null);
         setTitle("AeroBlue Platformer");
         setResizable(false);

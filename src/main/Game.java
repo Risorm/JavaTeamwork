6a55bc568342d67394dc88c895aca27a273de403
Velizar Bishurov <vbishurov@gmail.com>
2014-09-14 15:30:16 +0300

    bug fix

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,24 +1,23 @@
 package main;
+
 import javax.swing.JFrame;
 
-public class Game extends JFrame 
-{
-	
-	public static final int WIDTH = 320, HEIGHT = 240;
-	public static final int SCALE = 2;
-    public Game() 
-    {
-        add(new GamePanel());
+public class Game extends JFrame {
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(WIDTH*SCALE, HEIGHT*SCALE);
-        setLocationRelativeTo(null);
-        setTitle("AeroBlue Platformer");
-        setResizable(false);
-        setVisible(true);
-    }
+	public static final int WIDTH = 640, HEIGHT = 480;
 
-    public static void main(String[] args) {
-        new Game();
-    }
+	public Game() {
+		add(new GamePanel());
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(WIDTH, HEIGHT);
+		setLocationRelativeTo(null);
+		setTitle("AeroBlue Platformer");
+		setResizable(false);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Game();
+	}
 }

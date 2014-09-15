680ec9e5901f6f78b7f421a2fd9b669c95744deb
Krastyo <krastio.krastev@gmail.com>
2014-09-15 22:14:41 +0300

    background scrolling fix - not working good with foreground

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -18,28 +18,31 @@ import javax.swing.Timer;
 public class GamePanel extends JPanel implements ActionListener {
 
 	private Timer timer;
-	
+
 	Image background;
 	Image foreground;
 	Character character;
 	Enemy enemy;
-	int velx = 0, vely = 0, velx2 = 625;
+	int velx = 0, vely = 0, velx2 = 625, velx3 = 0;
 	Map map;
 	ArrayList<Coin> coins;
 	int startY = 0;
+
 	public GamePanel() {
 
 		addKeyListener(new InputHandler());
 		setFocusable(true);
 		setBackground(Color.BLACK);
 		setDoubleBuffered(true);
-		background = Toolkit.getDefaultToolkit().createImage("res/background.jpg");
+		background = Toolkit.getDefaultToolkit().createImage(
+				"res/background.jpg");
 
 		coins = new ArrayList<>();
-		coins.add(new Coin(5,20));
+		coins.add(new Coin(5, 20));
 		map = new Map();
-		foreground = Toolkit.getDefaultToolkit().createImage("res/foreground.png");
-		enemy = new Enemy(5,18,3);
+		foreground = Toolkit.getDefaultToolkit().createImage(
+				"res/foreground.png");
+		enemy = new Enemy(5, 18, 3);
 		character = new Character();
 		timer = new Timer(5, this);
 		timer.start();
@@ -49,72 +52,82 @@ public class GamePanel extends JPanel implements ActionListener {
 		super.paint(g);
 
 		Graphics2D graphics2d = (Graphics2D) g;
-		graphics2d.drawImage(background, 625-velx2, 0, null);
+
+		if ((character.rectangle.x - (background.getWidth(null) - Game.WIDTH))
+				% (background.getWidth(null)) == 0) {
+			velx3 = 0;
+		}
+		if ((character.rectangle.x - background.getWidth(null))
+				% (2 * background.getWidth(null)) == 0) {
+			velx2 = 0;
+		}
+		graphics2d.drawImage(background, 625 - velx2, 0, null);
+		if (character.rectangle.x > (background.getWidth(null) - Game.WIDTH)) {
+			graphics2d.drawImage(background, 625 - velx3, 0, null);
+		}
 		map.drawMap(graphics2d);
-		System.out.println(character.rectangle.x);		
-		for(Coin coin : coins)
-		{
+		System.out.println(character.rectangle.x);
+		for (Coin coin : coins) {
 			coin.drawCoin(graphics2d);
 		}
-		
+
 		enemy.drawEnemy(graphics2d);
 		graphics2d.drawImage(character.currentImage, character.rectangle.x,
 				character.rectangle.y, this);
-		graphics2d.drawImage(foreground, 625-velx2, 0, null);
-		
+		graphics2d.drawImage(foreground, 625 - velx2, 0, null);
+		if (character.rectangle.x > (foreground.getWidth(null) - Game.WIDTH)) {
+			graphics2d.drawImage(foreground, 625 - velx3, 0, null);
+		}
 		Toolkit.getDefaultToolkit().sync();
 		g.dispose();
 	}
 
 	public void actionPerformed(ActionEvent e) {
 		vely = 2;
-		if(character.isJumping == true)
-		{
+		if (character.isJumping == true) {
 			character.rectangle.y -= 8;
 			character.rectangle.x += velx;
 		}
-		if(character.rectangle.y <= startY - 2 * character.rectangle.height && character.isJumping == true)
-		{
+		if (character.rectangle.y <= startY - 2 * character.rectangle.height
+				&& character.isJumping == true) {
 			character.isJumping = false;
 			character.landing = true;
 		}
 		character.rectangle.x += velx;
 		velx2 += velx;
-		for(int i = 0; i < map.tiles.size();i++)
-		{
-			if(map.tiles.get(i).collidable == true)
-			{
-				if(character.rectangle.intersects(map.tiles.get(i).tileRectangle) && (character.walkingLeft == true || character.walkingRight == true))
-				{
+		velx3 += velx;
+
+		for (int i = 0; i < map.tiles.size(); i++) {
+			if (map.tiles.get(i).collidable == true) {
+				if (character.rectangle
+						.intersects(map.tiles.get(i).tileRectangle)
+						&& (character.walkingLeft == true || character.walkingRight == true)) {
 					character.rectangle.x -= velx;
 					velx2 -= velx;
-					
+					velx3 -= velx;
+
 				}
 			}
 		}
 		character.rectangle.y += vely;
-		for(int i = 0; i < map.tiles.size();i++)
-		{
-			if(map.tiles.get(i).collidable == true)
-			{
-				if(character.rectangle.intersects(map.tiles.get(i).tileRectangle))
-				{
+		for (int i = 0; i < map.tiles.size(); i++) {
+			if (map.tiles.get(i).collidable == true) {
+				if (character.rectangle
+						.intersects(map.tiles.get(i).tileRectangle)) {
 					character.canJump = true;
 					character.landing = false;
 					character.rectangle.y -= vely;
 				}
 			}
 		}
-		//animation
+		// animation
 		for (int i = 0; i < coins.size(); i++) {
-			if(character.rectangle.intersects(coins.get(i).rectangle))
-			{
+			if (character.rectangle.intersects(coins.get(i).rectangle)) {
 				coins.remove(coins.get(i));
 			}
 		}
-		
-		for(Coin coin : coins)
-		{
+
+		for (Coin coin : coins) {
 			coin.update();
 		}
 		enemy.update();
@@ -129,20 +142,20 @@ public class GamePanel extends JPanel implements ActionListener {
 			if (key == KeyEvent.VK_LEFT) {
 				character.idleRight = false;
 				character.idleLeft = false;
-				
+
 				character.walkingLeft = true;
 				character.walkingRight = false;
-				
+
 				velx = -1;
 			}
 
 			if (key == KeyEvent.VK_RIGHT) {
 				character.idleRight = false;
 				character.idleLeft = false;
-				
+
 				character.walkingLeft = false;
 				character.walkingRight = true;
-				
+
 				velx = 1;
 			}
 
@@ -151,7 +164,7 @@ public class GamePanel extends JPanel implements ActionListener {
 					character.isJumping = true;
 					character.canJump = false;
 					startY = character.rectangle.y;
-				} 
+				}
 			}
 		}
 
@@ -161,19 +174,18 @@ public class GamePanel extends JPanel implements ActionListener {
 			if (key == KeyEvent.VK_LEFT) {
 				character.idleRight = false;
 				character.idleLeft = true;
-				
+
 				character.walkingLeft = false;
 				character.jumpingLeft = false;
-				
+
 				velx = 0;
-			} 
-			else if (key == KeyEvent.VK_RIGHT) {
+			} else if (key == KeyEvent.VK_RIGHT) {
 				character.idleRight = true;
 				character.idleLeft = false;
-				
+
 				character.walkingRight = false;
 				character.jumpingRight = false;
-				
+
 				velx = 0;
 			}
 			if (key == KeyEvent.VK_UP) {
